b035d8430cdf58e9acfc44eb96b7145970cbc469
Aleksandr Parshukov <porshe97@yandex.ru>
2022-09-08 22:07:15 +0300

    2

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,6 @@
 $('document').ready(function () {
   $.getJSON(
-    'https://tools.aimylogic.com/api/googlesheet2json?sheet=%D0%9B%D0%B8%D1%81%D1%821&id=1kuBxfy3M7hPfpCtm7B7ZMnLPPCn4yK93z3E1m--X1KU',
+    'Access-Control-Allow-Origin:https://tools.aimylogic.com/api/googlesheet2json?sheet=%D0%9B%D0%B8%D1%81%D1%821&id=1kuBxfy3M7hPfpCtm7B7ZMnLPPCn4yK93z3E1m--X1KU',
     function (data) {
       var items = [];
       $.each(data, function (key, val) {
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 $('document').ready(function () {
+  console.log('ready');
   $.getJSON(
     'https://tools.aimylogic.com/api/googlesheet2json?sheet=%D0%9B%D0%B8%D1%81%D1%821&id=1kuBxfy3M7hPfpCtm7B7ZMnLPPCn4yK93z3E1m--X1KU',
     function (data) {
@@ -6,6 +7,8 @@ $('document').ready(function () {
       $.each(data, function (key, val) {
         items.push("<li id='" + key + "'>" + val + '</li>');
       });
+
+      console.log(data);
 
       $('<ul/>', {
         class: 'my-new-list',
